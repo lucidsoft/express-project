2694813883d14a35155f67cb9d27c21589a7f8d9
aromal <aromal@path-robotics.com>
2021-02-28 17:05:16 -0500

    serve static files using express

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use((req, res, next) => {
-  res.end("hello from express!");
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 module.exports = app;
